2bd87a518e28408c1161495195b979814cb70f91
Matija <matija.djuric@netplanet.rs>
2024-01-30 15:03:52 +0100

    cita dugmice

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,6 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.TimerScheduleModule.TimerScheduleHandler;
 import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+
+import java.util.Timer;
 
 public class Main {
 
@@ -10,6 +15,33 @@ public class Main {
         long timeStart = System.currentTimeMillis();
         var sp= SerialPort.getCommPort("demo");
 
+        SerialPort serialPort = SerialPort.getCommPort("COM11");
+        serialPort.setComPortParameters(9600,Byte.SIZE,SerialPort.ONE_STOP_BIT,SerialPort.NO_PARITY);
+        serialPort.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING,0,0);
+
+        try {
+            serialPort.openPort();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        serialPort.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() {
+                return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
+            }
+
+            @Override
+            public void serialEvent(SerialPortEvent serialPortEvent) {
+                if(serialPortEvent.getEventType()==SerialPort.LISTENING_EVENT_DATA_RECEIVED)
+                {
+                    String data = new String(serialPortEvent.getReceivedData());
+                    System.out.println(data);
+                }
+            }
+        });
 
     }
 }
